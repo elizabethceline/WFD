5250fad4ae315f7ce76355d11a725f8556ecf659
elizabethceline <elizabethceline04@gmail.com>
2024-08-27 16:09:47 +0700

    fix: change route

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/hello', function () {
     return view('hello');
